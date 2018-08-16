c2c72439d4e7b9603d405a443d0340821ee7d277
李仲翊 <cs102.larryleepig@ir.nctu.edu.tw>
2018-08-16 18:23:41 +0800

    實作channel,select

--- GOLANGLEARN/channel.go
+++ GOLANGLEARN/channel.go
@@ -8,10 +8,68 @@ import (
 func main() {
 	go f() //like thread parallize execute
 	time.Sleep(time.Microsecond * 1)
+	s := make(chan string) // 宣告一個 channel var
+
+	go func() {
+		s <- "hello"
+	}() // write in channel (sender)
+	val := <-s // read channel (receiver)
+	fmt.Println(val)
+
+	t := make(chan string, 1)
+	go func() {
+		for i := 0; i < 3; i++ {
+			fmt.Println("sender hello", i)
+			t <- fmt.Sprintf("receiver hello %d", i)
+		}
+	}()
+
+	for i := 0; i < 3; i++ {
+		val = <-t
+		fmt.Println(val)
+	}
+	//實作sellect
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		fmt.Println("c2 sender")
+		c2 <- "i an the two"
+	}()
+
+	go func() {
+		fmt.Println("c1 sender")
+		c1 <- "i am the one"
+	}()
+
+	for i := 0; i < 3; i++ {
+		select { // use to choose which chan in
+		case <-c1:
+			fmt.Println("fk")
+		case <-c2:
+			fmt.Println("cow")
+		default:
+			fmt.Println("nothing!")
+		}
+	}
+	//實作timeout
+	timeout := make(chan bool)
+
+	go func() {
+		time.Sleep(time.Second)
+		timeout <- true
+	}()
+
+	ch := make(chan int)
+	select {
+	case <-ch:
+	case <-timeout:
+		fmt.Println("\n\ntimeout!!!")
+	}
 }
 
 func f() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 2; i++ {
 		fmt.Println(i)
 	}
 }
