defe9fb0ed9560d3b3e05951e2f457bda0608a62
李仲翊 <cs102.larryleepig@ir.nctu.edu.tw>
2018-08-16 23:30:51 +0800

    learn panic recover

--- GOLANGLEARN/defer.go
+++ GOLANGLEARN/defer.go
@@ -3,12 +3,35 @@ package main
 import "fmt"
 
 //practice defer panic recover
-func test() (result int) {
+func test() (result string) {
 	defer func() {
-		result++
+		result = "not 0"
 	}()
-	return 0
+	return "0"
 }
+
+func LIFO() {
+	for i := 0; i < 4; i++ { //defer LIFO like stack
+		defer fmt.Println(i)
+	}
+}
+
 func main() {
 	fmt.Println(test())
+	LIFO()
+	defer func() {
+		fmt.Println("c")
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("d")
+	}()
+	f()
+}
+
+func f() {
+	fmt.Println("a")
+	panic("hello") //panic 以下的code不會執行 中間的值會傳到recover()
+	fmt.Println("b")
+	fmt.Println("f")
 }
